6e0a4b981b3b6bae2248639cb01dd9df4cf784e6
Gowsigan A <e18115@eng.pdn.ac.lk>
2022-11-09 19:58:01 +0530

    Finish lab03 exercises.

--- lab01/lab_ex_1.c
+++ lab01/lab_ex_1.c
@@ -1,34 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define BLINK_DELAY_MS 2000
+#define BLINK_DELAY_MS 1000
 
 int main(void)
 {
     DDRB = 0xFF;
-#include <avr/io.h>
-#include <util/delay.h>
 
-#define BLINK_DELAY_MS 1000
-
-    int main(void)
-    {
-        DDRB = 0xFF;
-
-        for (unsigned char i = 0; i < 256; i++)
-        {
-            PORTB = i;
-            _delay_ms(BLINK_DELAY_MS);
-        }
-
-        return 0;
-    }
-
-    PORTB = 0x00;
-
-    for (unsigned int i = 0; i < 256; i++)
+    for (unsigned char i = 0; i < 256; i++)
     {
         PORTB = i;
+        _delay_ms(BLINK_DELAY_MS);
     }
 
     return 0;
---
+++ lab03/example_1.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+// #include <util/delay.h>
+// #define BLINK_DELAY_MS 1000
+
+void delay_timer0()
+{
+    TCNT0 = 0x00; // load timer counter register with 0
+
+    TCCR0A = 0x00; // set the Timer0 under normal mode with no perscaler
+    TCCR0B = 0x01;
+
+    while ((TIFR0 & 0x01) == 0)
+        ; // wait till timer overflow bit (T0V0) is set
+
+    TCCR0A = 0x00; // clear Timer settings (this stops the timer)
+    TCCR0B = 0x00;
+
+    TIFR0 = 0x01; // clear the timer overflow bit (T0v0) for next round
+                  // strange thing about this flag is in order to clear it we should
+                  // write 1 to it This rule applies to all falgs of AVR chip
+}
+int main(void)
+{
+    DDRB = DDRB | (1 << 5); // configure pin 5 of PORTB for output
+
+    while (1)
+    {
+        PORTB = PORTB | (1 << 5); // set pin 5 high to turn led on
+        delay_timer0();
+
+        PORTB = PORTB & ~(1 << 5); // set pin 5 low to turn led off
+        delay_timer0();
+    }
+}
